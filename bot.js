899ce1de5591524fc5771fc84313bf7f0483fe0a
akefkinzi67-max <akefkinzi67@gmail.com>
2026-03-07 16:00:39 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,26 @@
 const { Client } = require('whatsapp-web.js')
 const qrcode = require('qrcode-terminal')
+const puppeteer = require('puppeteer')
 
 const client = new Client({
   puppeteer: {
+    headless: true,
+    executablePath: puppeteer.executablePath(),
     args: ['--no-sandbox','--disable-setuid-sandbox']
   }
 })
 
 client.on('qr', qr => {
+  console.log('QR RECEIVED')
   qrcode.generate(qr, { small: true })
 })
 
 client.on('ready', () => {
-  console.log('Bot is ready!')
+  console.log('Bot is ready')
 })
 
 client.on('message', msg => {
-  if (msg.body === 'hi') {
+  if (msg.body.toLowerCase() === 'hi') {
     msg.reply('Hello')
   }
 })
